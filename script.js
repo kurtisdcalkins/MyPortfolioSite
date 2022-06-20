04e1040e0bb470022f2e10e5998f6dc87d7457e0
Calkins Kurtis <kurtiscalkins@yahoo.com>
2022-06-20 14:03:36 -0600

    added extra styling

---
+++ script.js
@@ -0,0 +1,11 @@
+let project = document.querySelector('#each-project');
+// let projects = document.querySelector('#projects');
+
+// function viewProjects(event) {
+//     event.target.style.display = 'flex';
+
+// }
+
+// project.onmouseover = viewProjects;
+
+project.addEventListener('click', project.style.display = 'none');
