1490a7426fd5884e3731fed45064f59c8d563080
Calkins Kurtis <kurtiscalkins@yahoo.com>
2022-08-09 09:09:50 -0600

    Restructured the page

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-let project = document.querySelector('#each-project');
-let projects = document.querySelector('#projects');
-let arrows = document.querySelectorAll('.triangle-down');
 
-function viewProjects() {
-    project.style.display = 'flex';
-    arrows[0].hidden = true;
-    arrows[1].hidden = true;
-}
-
-projects.onmouseover = viewProjects;
